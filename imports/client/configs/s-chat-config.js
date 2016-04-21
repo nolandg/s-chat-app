f25715b6794cb64d72256f0dc1ab984c6bea01aa
Julian Ćwirko <julian.cwirko@gmail.com>
2016-04-21 19:21:02 +0200

    chatbox msg info

--- imports/client/configs/s-chat-config.js
+++ imports/client/configs/s-chat-config.js
@@ -6,7 +6,7 @@ import { sChat } from 'meteor/schat:client-core';
 Meteor.startup(function () {
     sChat.init('vkY7LM7w9YmtGpsGe', {
         ssl: true,
-        welcomeMessage: 'Hi, this is the demo chat box! You can have the same in your app! Do you have any questions?',
+        welcomeMessage: 'Hello, this is just a demo chat box, I\'m almost always offline ;) But if you have any questions, you can leave the message with your e-mail. Just send it as a message. I\'ll read it later and get back to you. There will be some improvements regarding this functionality soon!',
         hostName: 'www.simplechat.support',
         labels: {
             sendPlaceholder: 'Send the message... (Enter to send)',
