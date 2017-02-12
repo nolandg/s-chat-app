9adb986cd25adb23cd5598054737a21944a736e4
Noland <noland.germain@gmail.com>
2017-02-11 23:53:23 -0800

    Start Facebook Messenger notifications...

--- imports/server/notify.js
+++ imports/server/notify.js
@@ -1,3 +1,11 @@
+import { WebApp } from 'meteor/webapp';
+
+WebApp.connectHandlers.use('/fb-messenger', (req, res) => {
+  console.log(req.body);
+  res.writeHead(200);
+  res.end('Hello world');
+});
+
 function notifyAdmin() {
   console.log('Hey you!');
 }
