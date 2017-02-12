eba15b65c8ce47644c60ecc372ba9209f4c6631c
Noland <noland.germain@gmail.com>
2017-02-12 03:04:10 -0500

    Respond to FB webhook verification

--- imports/server/notify.js
+++ imports/server/notify.js
@@ -1,9 +1,15 @@
 import { WebApp } from 'meteor/webapp';
 
 WebApp.connectHandlers.use('/fb-messenger', (req, res) => {
-  console.log(req.body);
+  console.log('Processing webhook verification from Facebook...');
+  console.log('Token: ', req.query['hub.verify_token']);
+  console.log('Challenge: ', req.query['hub.challenge']);
   res.writeHead(200);
-  res.end('Hello world');
+  if(req.query['hub.verify_token'] === 'peachypaws'){
+    res.end(req.query['hub.challenge']);
+  }else{
+   res.end('Not peachy. Sorry.');
+  }
 });
 
 function notifyAdmin() {
